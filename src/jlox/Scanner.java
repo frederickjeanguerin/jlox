9631294fbe2c63425e795cacd06c257854669ffb
Guerin Frederic <fguerin@cstjean.qc.ca>
2022-06-03 03:00:08 -0400

    Chapter 4 - Scanning (done)

---
+++ src/jlox/Scanner.java
@@ -0,0 +1,228 @@
+package jlox;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import static java.util.Map.entry;
+import static jlox.TokenType.*;
+
+class Scanner {
+    private final String source;
+    private final List<Token> tokens = new ArrayList<>();
+    private int start = 0;      // offset in source
+    private int current = 0;    // offset in source
+    private int line = 1;
+
+    Scanner(String source) {
+        this.source = source;
+    }
+
+    List<Token> scanTokens() {
+        while (!isAtEnd()) {
+            // we are at the beginning of the next lexeme
+            start = current;
+            scanToken();
+        }
+        tokens.add(new Token(EOF, "", null, line));
+        return tokens;
+    }
+
+    private boolean isAtEnd() {
+        return current >= source.length();
+    }
+
+    private void scanToken() {
+        char c = advance();
+        switch (c) {
+            // Single char tokens
+            case '(':
+                addToken(LEFT_PAREN);
+                break;
+            case ')':
+                addToken(RIGHT_PAREN);
+                break;
+            case '{':
+                addToken(LEFT_BRACE);
+                break;
+            case '}':
+                addToken(RIGHT_BRACE);
+                break;
+            case ',':
+                addToken(COMMA);
+                break;
+            case '.':
+                addToken(DOT);
+                break;
+            case '-':
+                addToken(MINUS);
+                break;
+            case '+':
+                addToken(PLUS);
+                break;
+            case ';':
+                addToken(SEMICOLON);
+                break;
+            case '*':
+                addToken(STAR);
+                break;
+
+            // Single or double char tokens
+            case '!':
+                addToken(match('=') ? BANG_EQUAL : BANG);
+                break;
+            case '=':
+                addToken(match('=') ? EQUAL_EQUAL : EQUAL);
+                break;
+            case '<':
+                addToken(match('=') ? LESS_EQUAL : LESS);
+                break;
+            case '>':
+                addToken(match('=') ? GREATER_EQUAL : GREATER);
+                break;
+
+            // slash or comment
+            case '/':
+                if (match('/')) {
+                    // Line comments
+                    advanceAfter('\n');
+                } else {
+                    addToken(SLASH);
+                }
+                break;
+
+            // Whites
+            case ' ':
+            case '\n':
+            case '\t':
+            case '\r':
+                // Ignore
+                break;
+
+            case '"':
+                string();
+                break;
+
+            default:
+                if (isDigit(c)) {
+                    number();
+                } else if (isAlpha(c)) {
+                    identifier();
+                } else {
+                    // TODO Collapse consecutive unexpected chars together in a single error message.
+                    Lox.error(line, "Unexpected character '" + c + "'");
+                }
+        }
+    }
+
+    private boolean isDigit(char c) {
+        return '0' <= c && c <= '9';
+    }
+
+    private boolean isAlpha(char c) {
+        return 'a' <= c && c <= 'z'
+            || 'A' <= c && c <= 'Z'
+            || c == '_';
+    }
+
+    private boolean isAlphaNumeric(char c) {
+        return isDigit(c) || isAlpha(c);
+    }
+
+    private char advance() {
+        char c = source.charAt(current++);
+        if (c == '\n') line++;
+        return c;
+    }
+
+    private boolean advanceAfter(char expected) {
+        while (peek() != expected) {
+            if (isAtEnd()) return false;
+            advance();
+        }
+        advance();
+        return true;
+    }
+
+    private boolean match(char expected) {
+        if (peek() != expected)
+            return false;
+        advance();
+        return true;
+    }
+
+    private char peek() {
+        return peek(0);
+    }
+
+    private char peek(int ahead) {
+        if (current + ahead < source.length())
+            return source.charAt(current + ahead);
+        return '\0';
+    }
+
+    private void string() {
+        // TODO Add escape sequences or at least the possibility to insert a " inside a string
+        if (!advanceAfter('"')) {
+            Lox.error(line, "Unterminated string");
+        } else {
+            String value = source.substring(start + 1, current - 1);
+            addToken(STRING, value);
+        }
+    }
+
+    private void number() {
+        while (isDigit(peek())) advance();
+
+        if (peek() == '.' && isDigit(peek(1)))
+            advance();
+
+        while (isDigit(peek())) advance();
+
+        String numeric = source.substring(start, current);
+
+        // NB parseDouble will succeed here
+        addToken(NUMBER, Double.parseDouble(numeric));
+    }
+
+    private void identifier() {
+        while (isAlphaNumeric(peek())) advance();
+        String text = source.substring(start, current);
+        var type = keywords.get(text);
+        if (type == null)
+            type = IDENTIFIER;
+        addToken(type);
+    }
+
+    private void addToken(TokenType type) {
+        addToken(type, null);
+    }
+
+    private void addToken(TokenType type, Object literal) {
+        String text = source.substring(start, current);
+        tokens.add(new Token(type, text, literal, line));
+    }
+
+    private static final Map<String, TokenType> keywords;
+
+    static {
+        keywords = Map.ofEntries(
+                entry("and", AND),
+                entry("class", CLASS),
+                entry("else", ELSE),
+                entry("false", FALSE),
+                entry("for", FOR),
+                entry("fun", FUN),
+                entry("if", IF),
+                entry("nil", NIL),
+                entry("or", OR),
+                entry("print", PRINT),
+                entry("return", RETURN),
+                entry("super", SUPER),
+                entry("this", THIS),
+                entry("true", TRUE),
+                entry("var", VAR),
+                entry("while", WHILE)
+        );
+    }
+}
---
+++ src/jlox/Token.java
@@ -0,0 +1,7 @@
+package jlox;
+
+record Token(TokenType type, String lexeme, Object literal, int line) {
+    public String toString() {
+        return type + " " + lexeme + " " + literal;
+    }
+}
