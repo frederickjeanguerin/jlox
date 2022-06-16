760eb1fbd398f2deddb8ee36fecc6048bf126b37
Guerin Frederic <fguerin@cstjean.qc.ca>
2022-06-16 19:43:29 -0400

    Chapter 12.3 - Class instantiation (tested)

--- src/jlox/LoxClass.java
+++ src/jlox/LoxClass.java
@@ -1,6 +1,8 @@
 package jlox;
 
-public class LoxClass {
+import java.util.List;
+
+public class LoxClass implements LoxCallable {
     final String name;
 
     public LoxClass(String name) {
@@ -10,5 +12,15 @@ public class LoxClass {
     @Override
     public String toString() {
         return "<class %s>".formatted(name);
+    }
+
+    @Override
+    public int arity() {
+        return 0;
+    }
+
+    @Override
+    public Object call(Interpreter interpreter, Token leftPar, List<Object> arguments) {
+        return new LoxInstance(this);
     }
 }
